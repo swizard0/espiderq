702242a0f38c6c1c0c7cdf0da45e154bcc5f2c5e
Alexey Voznyuk <me@swizard.info>
2015-11-10 00:09:33 +0300

    support stats protocol cmd and reply

--- src/proto.erl
+++ src/proto.erl
@@ -12,7 +12,9 @@ encode({repay, Id, penalty}) ->
 encode({repay, Id, reward}) ->
     <<16#01:8, 16#04:8, Id:32/big, 16#02:8>>;
 encode({repay, Id, front}) ->
-    <<16#01:8, 16#04:8, Id:32/big, 16#03:8>>.
+    <<16#01:8, 16#04:8, Id:32/big, 16#03:8>>;
+encode(stats) ->
+    <<16#01:8, 16#05:8>>.
 
 decode(<<16#01:8, 16#01:8, Count:32/big>>) ->
     {counted, Count};
@@ -22,5 +24,7 @@ decode(<<16#01:8, 16#03:8, Id:32/big, Data/binary>>) ->
     {lent, Id, Data};
 decode(<<16#01:8, 16#04:8>>) ->
     repaid;
+decode(<<16#01:8, 16#05:8, Count:64/big, Add:64/big, Lend:64/big, Repay:64/big, Stats:64/big>>) ->
+    {stats, {count, Count}, {add, Add}, {lend, Lend}, {repay, Repay}, {stats, Stats}};
 decode(Unrecognized) ->
     {decode_error, Unrecognized}.
