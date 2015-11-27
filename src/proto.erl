f01eee170376a86c8f520a10930113fe1fe838d0
Alexey Voznyuk <me@swizard.info>
2015-11-27 17:42:31 +0300

    support lend modes and queue_empty response

--- src/proto.erl
+++ src/proto.erl
@@ -1,14 +1,15 @@
 -module(proto).
 -export([encode/1, decode/1]).
 
-encode(count) -> 
+encode(count) ->
     <<16#01:8>>;
 encode({add, Key, Value}) ->
     <<16#02:8, (byte_size(Key)):32/big, Key/binary, (byte_size(Value)):32/big, Value/binary>>;
 encode({update, Key, Value}) ->
     <<16#03:8, (byte_size(Key)):32/big, Key/binary, (byte_size(Value)):32/big, Value/binary>>;
-encode({lend, TimeoutMs}) ->
-    <<16#04:8, TimeoutMs:64/big>>;
+encode({lend, TimeoutMs, Mode}) ->
+    ModeBin = case Mode of block -> 1; poll -> 2 end,
+    <<16#04:8, TimeoutMs:64/big, ModeBin:8>>;
 encode({repay, LendKey, Key, ChangedValue, Status}) ->
     StatusBin = case Status of penalty -> 1; reward -> 2; front -> 3; drop -> 4 end,
     <<16#05:8, LendKey:64/big, (byte_size(Key)):32/big, Key/binary, (byte_size(ChangedValue)):32/big, ChangedValue/binary, StatusBin:8>>;
@@ -31,6 +32,8 @@ decode(<<16#05:8>>) ->
     not_found;
 decode(<<16#06:8, LendKey:64/big, KeyLen:32/big, Key:KeyLen/binary, ValueLen:32/big, Value:ValueLen/binary>>) ->
     {lent, LendKey, Key, Value};
+decode(<<16#10:8>>) ->
+    queue_empty;
 decode(<<16#07:8>>) ->
     repaid;
 decode(<<16#08:8>>) ->
