a88a6fd253c0d96396b2cba3ee66142989773595
Alexey Voznyuk <me@swizard.info>
2015-11-28 02:18:31 +0300

    full proto sync

--- src/proto.erl
+++ src/proto.erl
@@ -7,6 +7,8 @@ encode({add, Key, Value}) ->
     <<16#02:8, (byte_size(Key)):32/big, Key/binary, (byte_size(Value)):32/big, Value/binary>>;
 encode({update, Key, Value}) ->
     <<16#03:8, (byte_size(Key)):32/big, Key/binary, (byte_size(Value)):32/big, Value/binary>>;
+encode({lookup, Key}) ->
+    <<16#09:8, (byte_size(Key)):32/big, Key/binary>>;
 encode({lend, TimeoutMs, Mode}) ->
     ModeBin = case Mode of block -> 1; poll -> 2 end,
     <<16#04:8, TimeoutMs:64/big, ModeBin:8>>;
@@ -17,6 +19,8 @@ encode({heartbeat, LendKey, Key, TimeoutMs}) ->
     <<16#06:8, LendKey:64/big, (byte_size(Key)):32/big, Key/binary, TimeoutMs:64/big>>;
 encode(stats) ->
     <<16#07:8>>;
+encode(flush) ->
+    <<16#0A:8>>;
 encode(terminate) ->
     <<16#08:8>>.
 
@@ -30,6 +34,10 @@ decode(<<16#04:8>>) ->
     updated;
 decode(<<16#05:8>>) ->
     not_found;
+decode(<<16#0D:8, ValueLen:32/big, Value:ValueLen/binary>>) ->
+    {value_found, Value};
+decode(<<16#0E:8>>) ->
+    value_not_found;
 decode(<<16#06:8, LendKey:64/big, KeyLen:32/big, Key:KeyLen/binary, ValueLen:32/big, Value:ValueLen/binary>>) ->
     {lent, LendKey, Key, Value};
 decode(<<16#10:8>>) ->
@@ -38,6 +46,8 @@ decode(<<16#07:8>>) ->
     repaid;
 decode(<<16#08:8>>) ->
     heartbeaten;
+decode(<<16#0D:8>>) ->
+    flushed;
 decode(<<16#0A:8, Count:64/big, Add:64/big, Update:64/big, Lookup:64/big, Lend:64/big, Repay:64/big, Heartbeat:64/big, Stats:64/big>>) ->
     {stats_got, [{count, Count},
                  {add, Add},
